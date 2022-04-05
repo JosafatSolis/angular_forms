2ebdcf4ee84b1605d2021ff83819bab97a38f1c6
jsolis <jsolis@macroaplicaciones.com>
2022-04-04 18:53:03 -0700

    Commit inicial

---
+++ forms_solis/src/app/components/fomulario/fomulario.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FomularioComponent } from './fomulario.component';
+
+describe('FomularioComponent', () => {
+  let component: FomularioComponent;
+  let fixture: ComponentFixture<FomularioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FomularioComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FomularioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
